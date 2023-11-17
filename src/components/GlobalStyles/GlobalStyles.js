5adedeb11b1170de3df50fd578da2ad0fe737353
almudenaredradix <almudena@redradix.com>
2023-11-17 11:14:17 +0100

    :lipstick: Optimize the variable font file and host it on our server

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -18,39 +18,38 @@ article, aside, canvas, details, embed,
 figure, figcaption, footer, header, hgroup,
 menu, nav, output, ruby, section, summary,
 time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	vertical-align: baseline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
 article, aside, details, figcaption, figure,
 footer, header, hgroup, menu, nav, section {
-	display: block;
+  display: block;
 }
 body {
-	line-height: 1;
+  line-height: 1;
 }
 ol, ul {
-	list-style: none;
+  list-style: none;
 }
 blockquote, q {
-	quotes: none;
+  quotes: none;
 }
 blockquote:before, blockquote:after,
 q:before, q:after {
-	content: '';
-	content: none;
+  content: '';
+  content: none;
 }
 table {
-	border-collapse: collapse;
-	border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
 * {
   box-sizing: border-box;
-  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   font-synthesis: none;
 }
@@ -72,6 +71,12 @@ html {
   --color-text: var(--color-gray-1000);
   --color-background: white;
 }
+
+body {
+  font-family: 'Inter', sans-serif;
+}
 `;
+
+
 
 export default GlobalStyles;
